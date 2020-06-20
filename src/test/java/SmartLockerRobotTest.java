3105d8831c2e0ae192a27745f9c4f354bce70e46
i5possible <lianghongbuaa@gmail.com>
2020-06-20 21:52:57 +0800

    should return the same bag when pick up from smart locker robot given save bag to primary locker robot

--- src/test/java/SmartLockerRobotTest.java
+++ src/test/java/SmartLockerRobotTest.java
@@ -42,11 +42,26 @@ public class SmartLockerRobotTest {
     }
 
     @Test
-    public void should_return_the_same_bag_when_pick_up_bag_given_valid_ticket() {
+    public void should_get_the_bag_when_pick_up_bag_given_valid_ticket() {
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(new Locker(0), new Locker(1), new Locker(0));
 
         Bag bag = new Bag();
         Ticket ticket = smartLockerRobot.save(bag);
+
+        Bag returnedBag = smartLockerRobot.pickUp(ticket);
+        assertEquals(bag, returnedBag);
+    }
+
+    @Test
+    public void should_get_the_bag_when_pick_up_bag_from_smart_locker_robot_given_save_bag_to_primary_locker_robot() {
+        Locker firstLocker = new Locker(1);
+        Locker secondLocker = new Locker(2);
+        Locker thirdLocker = new Locker(3);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(firstLocker, secondLocker, thirdLocker);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(firstLocker, secondLocker, thirdLocker);
+
+        Bag bag = new Bag();
+        Ticket ticket = primaryLockerRobot.save(bag);
 
         Bag returnedBag = smartLockerRobot.pickUp(ticket);
         assertEquals(bag, returnedBag);
