714af78bbc0964627a141613ad0cca025f5bfcff
hong.liang <lianghongbuaa@gmail.com>
2020-07-01 12:46:31 +0800

    locker robot director manage 1 manager with 2 lockers can view report

--- src/test/java/LockerRobotDirectorTest.java
+++ src/test/java/LockerRobotDirectorTest.java
@@ -18,4 +18,29 @@ public class LockerRobotDirectorTest {
                 "  L 1 2";
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void should_return_report_when_view_report_given_director_manage_1_manager_with_2_lockers() {
+        final Locker firstLocker = createLocker(5, 2);
+        final Locker secondLocker = createLocker(5, 2);
+        final LockerRobotManager lockerRobotManager = new LockerRobotManager(List.of(firstLocker, secondLocker), List.of());
+
+        final LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(List.of(lockerRobotManager));
+
+        final String actual = lockerRobotDirector.viewReport();
+        String expected = "M 4 10\n" +
+                "  L 2 5\n" +
+                "  L 2 5";
+        assertEquals(expected, actual);
+    }
+
+    private Locker createLocker(int totalCapacity, int availableCapacity) {
+        final Locker locker = new Locker(totalCapacity);
+        int bagsCount = totalCapacity - availableCapacity;
+        while(bagsCount > 0) {
+            locker.save(new Bag());
+            bagsCount--;
+        }
+        return locker;
+    }
 }
